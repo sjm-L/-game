473fe5ddb046f384c8c1a03af67f734c4aa145f7
손정민 <shower1300@gmail.com>
2025-06-24 23:22:03 +0900

    commit

--- Assets/NewEmptyCSharpScript.cs
+++ Assets/NewEmptyCSharpScript.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class NewEmptyCSharpScript : MonoBehaviour
 {
-    
+    void Start()
+    {
+        Debug.Log("Hello Unity!");
+    }
 }
